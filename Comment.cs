937de9d5464a47b379a7ebda7152deeb70c6c3fb
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2021-05-06 11:17:07 +0200

    jnjbjhbhb

---
+++ Comment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SemProComment2
+{
+    public class Comment
+    {
+        private string _commenttext;
+        private int _id;
+
+
+        public string Commenttext
+        {
+            get => _commenttext;
+            set
+            {
+                _commenttext = value;
+            }
+        }
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+            }
+        }
+        public Comment(int Id, string Commenttext)
+        {
+            _commenttext = Commenttext;
+            _id = Id;
+        }
+        public Comment()
+        {
+
+        }
+    }
+}
